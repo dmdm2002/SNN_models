3595633c996ef9eb6a873ed650583ed9f73e4597
JungSooKim <61766408+dmdm2002@users.noreply.github.com>
2023-10-04 12:34:32 +0900

    Update main.py

--- SpikingUNet/main.py
+++ SpikingUNet/main.py
@@ -2,6 +2,6 @@ from Runner.Train import Trainer
 import wandb
 
 if __name__ == '__main__':
-    wandb.login(key='2f90610010c90fee7d521748a5c0d2d17db87255')
+    wandb.login(key='')
     a = Trainer()
     a.run()
